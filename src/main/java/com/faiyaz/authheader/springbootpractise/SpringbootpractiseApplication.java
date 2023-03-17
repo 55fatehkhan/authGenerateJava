35b113b9a7921f429245b15d4c47854c394bd4ce
Faiyaz <faiyazsir401@gmail.com>
2023-03-17 07:52:46 +0530

    first commit

---
+++ src/main/java/com/faiyaz/authheader/springbootpractise/SpringbootpractiseApplication.java
@@ -0,0 +1,14 @@
+package com.faiyaz.authheader.springbootpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpractiseApplication.class, args);
+		System.out.println("Program starts from here..........");
+	}
+
+}
---
+++ src/test/java/com/faiyaz/authheader/springbootpractise/SpringbootpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faiyaz.authheader.springbootpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
